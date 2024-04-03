39fbfe9b3c0e8c440fa5261f59f44895312f4f7e
Jason <ezp9@txstate.edu>
2024-04-03 14:17:19 -0500

    Updated UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,25 @@ import ProtectedRoute from "./routeguard.jsx";
 import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
 import Dashboard from "./dashboard/dashboard";
 import Callback from "./callback.jsx";
+import CreatePlaylist from "./components/createplaylist.jsx";
 
 const App = () => {
   return (
-      <BrowserRouter>
-        <div className="header">
-          <img src=""></img> {/* for settings cog */}
-        </div>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Route>
-          <Route path="/callback" element={<Callback />} />
-          <Route path="/" element={<Navigate replace to="/login" />} />
-          <Route path="*" element={<Navigate replace to="/login" />} />
-        </Routes>
-        <div className="footer"></div>
-      </BrowserRouter>
+    <BrowserRouter>
+      <div className="header">
+        <img src=""></img> {/* for settings cog */}
+      </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<CreatePlaylist />} />
+        </Route>
+        <Route path="/callback" element={<Callback />} />
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="*" element={<Navigate replace to="/login" />} />
+      </Routes>
+      <div className="footer"></div>
+    </BrowserRouter>
   );
 };
 
