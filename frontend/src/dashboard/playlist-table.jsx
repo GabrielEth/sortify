05e0211b37309be13265460165beda6cbb70a597
Stormie Renevey <sarenevey@icloud.com>
2024-04-03 21:29:02 -0500

    Buttons & Routing!!

--- frontend/src/dashboard/playlist-table.jsx
+++ frontend/src/dashboard/playlist-table.jsx
@@ -2,19 +2,30 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import { useNavigate } from 'react-router-dom';
 
 import "./playlist-table.css";
-
 import newPlaylist from "../../../Resources/newPlaylist.png";
 
 const PlaylistTable = ({ playlists }) => {
   const cardsPerRow = 5;
+  const navigate = useNavigate();
+
+  // Navigate to the create playlist page
+  const handleCreateNewPlaylistClick = () => {
+    navigate('/createplaylist');
+  };
+
+  // Navigate to the update playlist page and pass the playlist name
+  const handlePlaylistClick = (playlistName) => {
+    navigate('/updateplaylist', { state: { playlistName } });
+  };
 
   return (
     <Container>
       <Row className="g-3">
         {/* Placeholder for "Create New Playlist" card */}
-        <Col md={12 / cardsPerRow}>
+        <Col md={12 / cardsPerRow} onClick={handleCreateNewPlaylistClick} style={{ cursor: 'pointer' }}>
           <Card className="card-hover-effect">
             <Card.Img
               variant="top"
@@ -29,7 +40,7 @@ const PlaylistTable = ({ playlists }) => {
 
         {/* Dynamic playlist cards */}
         {playlists.map((playlist, index) => (
-          <Col key={index} md={12 / cardsPerRow}>
+          <Col key={index} md={12 / cardsPerRow} onClick={() => handlePlaylistClick(playlist.name)} style={{ cursor: 'pointer' }}>
             <Card className="card-hover-effect">
               <Card.Img
                 variant="top"
