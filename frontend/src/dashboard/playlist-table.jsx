e552ec40df8011f4d5f1f5578e52ab1fff3f0505
Stormie Renevey <sarenevey@icloud.com>
2024-04-03 21:38:38 -0500

    Fixed Card Sizing

--- frontend/src/dashboard/playlist-table.jsx
+++ frontend/src/dashboard/playlist-table.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -11,12 +12,10 @@ const PlaylistTable = ({ playlists }) => {
   const cardsPerRow = 5;
   const navigate = useNavigate();
 
-  // Navigate to the create playlist page
   const handleCreateNewPlaylistClick = () => {
     navigate('/createplaylist');
   };
 
-  // Navigate to the update playlist page and pass the playlist name
   const handlePlaylistClick = (playlistName) => {
     navigate('/updateplaylist', { state: { playlistName } });
   };
@@ -24,29 +23,18 @@ const PlaylistTable = ({ playlists }) => {
   return (
     <Container>
       <Row className="g-3">
-        {/* Placeholder for "Create New Playlist" card */}
-        <Col md={12 / cardsPerRow} onClick={handleCreateNewPlaylistClick} style={{ cursor: 'pointer' }}>
+        <Col md={12 / cardsPerRow} className="playlist-card" onClick={handleCreateNewPlaylistClick} style={{ cursor: 'pointer' }}>
           <Card className="card-hover-effect">
-            <Card.Img
-              variant="top"
-              src={newPlaylist}
-              alt="Create New Playlist"
-            />
+            <Card.Img variant="top" src={newPlaylist} alt="Create New Playlist" />
             <Card.Body>
               <Card.Text>Create New Playlist</Card.Text>
             </Card.Body>
           </Card>
         </Col>
-
-        {/* Dynamic playlist cards */}
         {playlists.map((playlist, index) => (
-          <Col key={index} md={12 / cardsPerRow} onClick={() => handlePlaylistClick(playlist.name)} style={{ cursor: 'pointer' }}>
+          <Col key={index} md={12 / cardsPerRow} className="playlist-card" onClick={() => handlePlaylistClick(playlist.name)} style={{ cursor: 'pointer' }}>
             <Card className="card-hover-effect">
-              <Card.Img
-                variant="top"
-                src={playlist.imageUrl || "path/to/your/placeholder_image.jpg"}
-                alt={playlist.name}
-              />
+              <Card.Img variant="top" src={playlist.imageUrl || "path/to/your/placeholder_image.jpg"} alt={playlist.name} />
               <Card.Body>
                 <Card.Title>{playlist.name}</Card.Title>
                 <Card.Text>{playlist.description}</Card.Text>
