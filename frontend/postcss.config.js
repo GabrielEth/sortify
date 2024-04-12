dc1b35214dd7c590abcccd5ce761d1f926c2af43
Dylan Bladen <dylanwbladen@gmail.com>
2024-04-12 16:02:20 -0500

    Revert "purgecss"
    This reverts commit e9822fec8caac725386f1e68a646bcd32f52ef2d.

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,13 +1,6 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-import purgecss from '@fullhuman/postcss-purgecss';
-
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-    purgecss({
-      content: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx'],
-    }),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
