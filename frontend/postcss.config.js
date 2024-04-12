e9822fec8caac725386f1e68a646bcd32f52ef2d
Dylan Bladen <dylanwbladen@gmail.com>
2024-04-12 15:58:30 -0500

    purgecss

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,13 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import purgecss from '@fullhuman/postcss-purgecss';
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    purgecss({
+      content: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx'],
+    }),
+  ],
 }
