92d69d3995749ff09ac1be9ba8c309d2f958c431
Stormie Renevey <sarenevey@icloud.com>
2024-04-15 17:55:16 -0500

    Loading U/I Update

--- frontend/src/loading-circle.jsx
+++ frontend/src/loading-circle.jsx
@@ -1,19 +1,20 @@
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography'; // Import Typography for text
+import Typography from '@mui/material/Typography'; 
 
 export default function CircularIndeterminate() {
   return (
     <Box sx={{ 
       display: 'flex', 
-      flexDirection: 'column',  // Stack the progress circle and text vertically
-      alignItems: 'center',     // Center align items horizontally
-      justifyContent: 'center', // Center align items vertically
-      height: '100vh',          // Take full viewport height
+      flexDirection: 'column', 
+      alignItems: 'center',   
+      justifyContent: 'center', 
+      height: '100vh',         
+      color: '#52b788',        
     }}>
-      <CircularProgress />
-      <Typography variant="h6" sx={{ mt: 2 }}> // Add some margin top for spacing
-        Importing user's Spotify data...
+      <CircularProgress size={60} color="inherit" />  
+      <Typography variant="h4" sx={{ mt: 3, fontFamily: 'Comic Sans MS', color: 'inherit' }}>
+        Getting your music!
       </Typography>
     </Box>
   );
